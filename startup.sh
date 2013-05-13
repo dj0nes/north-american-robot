435df74fc07a618422357b6c28ba5bd17be79e9e
Dillon Jones <dillon.z.jones@gmail.com>
2013-05-13 04:06:51 -0400

    A great startup script.
    Signed-off-by: Dillon Jones <dillon.z.jones@gmail.com>

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#Mount Sonics:
+/usr/bin/udisks --mount /dev/disk/by-uuid/A0E4B07EE4B057EC
+
+#Mount Nero:
+/usr/bin/udisks --mount /dev/disk/by-uuid/AC58974B58971366
+
+#Mount LinuxSteam:
+/usr/bin/udisks --mount /dev/disk/by-uuid/9107d31c-a6e6-4190-9484-e23ba364956f
